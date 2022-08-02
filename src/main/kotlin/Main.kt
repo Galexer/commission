d65c9ad4835025db6b3bf34477d0394235e4dc67
Galina Ry <galya.rybynok@gmail.com>
2022-08-02 22:55:29 +0300

    fun commission

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,24 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+package ru.netology
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+import kotlin.math.ceil
+
+fun main() {
+    val sum = 1000_00
+    val previousPaym = 0
+    val typeCard = "VK pay"
+    println( commission(sum, previousPaym, typeCard))
+}
+
+fun commission(sum: Int, previousPaym: Int, typeCard: String): Int {
+    val mmCardLimit = 75000_00
+    val mmCardChengeCommis = 0.006
+    val mmCardStaticCommis = 20_00
+    val visaMirMinCommis = 35_00
+    val visaMirCommis = 0.0075
+    when (typeCard) {
+        "Mastercard", "Maestro" -> return if(previousPaym <= mmCardLimit) 0 else ceil((sum * mmCardChengeCommis) - mmCardStaticCommis).toInt()
+        "VK pay" -> return 0
+        "Visa", "Мир" -> return if(sum * visaMirCommis < visaMirMinCommis) visaMirMinCommis else ceil(sum * visaMirCommis).toInt()
+    }
+    return -1
 }
